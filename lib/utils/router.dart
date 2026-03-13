c905fe61ade7edb1324f36ec00bf942e6e37ae91
AlihanSDev <ali70s@mail.ru>
2026-03-13 11:58:57 +0600

    feat-router

---
+++ lib/utils/router.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../views/films_list_screen.dart';
+import '../views/film_detail_screen.dart';
+
+final GoRouter router = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) {
+        final selectedFilmId = state.uri.queryParameters['filmId'];
+        return FilmsListScreen(
+          selectedFilmId: selectedFilmId != null ? int.parse(selectedFilmId) : null,
+        );
+      },
+      routes: [
+        GoRoute(
+          path: 'film/:id',
+          builder: (context, state) {
+            final filmId = int.parse(state.pathParameters['id']!);
+            return FilmDetailScreen(filmId: filmId);
+          },
+        ),
+      ],
+    ),
+  ],
+);
